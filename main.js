1c93b7d551e443bcf512af8241cc9fabe5f826f7
erkaerka21 <eegiibaagii597@gmail.com>
2024-06-11 12:54:28 +0800

    6.11 nii udur JScondition

---
+++ main.js
@@ -0,0 +1,142 @@
+/* let weeknumber = Number(prompt("Та 1 - 7 хооронд тоо сонгож оруулна уу !!!"));
+let garigiinNer;
+
+if (weeknumber === 1) {
+  //   console.log("Даваа");
+  garigiinNer = "Даваа";
+} else if (weeknumber === 2) {
+  //   console.log("Мягмар");
+  garigiinNer = "Мягмар";
+} else if (weeknumber === 3) {
+  //   console.log("Лхагва");
+  garigiinNer = "Лхагва";
+} else if (weeknumber === 4) {
+  //   console.log("Пүрэв");
+  garigiinNer = "Пүрэв";
+} else if (weeknumber === 5) {
+  //   console.log("Баасан");
+  garigiinNer = "Баасан";
+} else if (weeknumber === 6) {
+  //   console.log("Бямба");
+  garigiinNer = "Бямба";
+// } else if (weeknumber === 7) {
+//   //   console.log("Ням");
+//   garigiinNer = "Ням";
+// } else console.log("Таны оруулсан тоонд тохирох гаригийн нэр олдсонгүй.");
+
+// console.log("Таны оруулсан тоо : ", weeknumber);
+// console.log("Таны оруулсан тоонд тохирох гаригийн нэр : ", garigiinNer); */
+
+// // let age = 20;
+// // let gender = "эр";
+
+// // if (age >= 18 || gender === "эр") {
+// //   console.log("Та насанл хүрсэн эсвэл эрэгтэй");
+// // } else {
+// //   console.log("Та насанд хүрээгүй болон эмэгтэй байна");
+// }
+
+// let weeknumber = Number(prompt("Та 1 - 7 хооронд тоо сонгож оруулна уу !!!"));
+// let garigiinNer;
+// switch(weeknumber) {
+//     case 1 : {
+//         garigiinNer = "Даваа"
+//         break;
+//     }
+//     case 2 : {
+//         garigiinNer = "Мягмар"
+//         break;
+//     }
+//     case 3 : {
+//         garigiinNer = "Лхагва"
+//         break;
+//     }
+//     case 4 : {
+//         garigiinNer = "Пүрэв"
+//         break;
+//     }
+//     case 5 : {
+//         garigiinNer = "Баасан"
+//         break;
+//     }
+//     case 6 : {
+//         garigiinNer = "Бямба"
+//         break;
+//     }
+//     case 7 : {
+//         garigiinNer = "Ням"
+//         break;
+//     }
+//     default : "Олдсонгүй"
+// }
+
+// console.log("Таны оруулсан тоо : ", weeknumber);
+// console.log("Таны оруулсан тоонд тохирох гаригийн нэр : ", garigiinNer);
+
+//daalgavar 1
+let myAge = 26;
+if (myAge > 25) {
+  console.log("above 25", myAge);
+} else {
+  console.log("below 25", myAge);
+}
+
+//daalgavar 2
+let n = 17;
+if (n % 2) {
+  console.log("сондгой тоо", n);
+} else {
+  console.log("тэгш тоо", n);
+}
+
+//daalgavar 3
+let X = 12;
+let Y = 45;
+let Z = 30;
+if (Y > X) {
+  console.log("хамгийн их утга : ", Y);
+} else if (X > Y) {
+  console.log("хамгийн их утга : ", X);
+} else if (Z > Y) {
+  console.log("хамгийн их утга : ", Z);
+} else if (Z > X) {
+  console.log("хамгийн их утга : ", Z);
+}
+
+//daalgavar 4
+let A = 12;
+let B = 45;
+let C = 30;
+if (A < B) {
+  console.log("хамгийн бага утга : ", A);
+} else if (B < A) {
+  console.log("хамгийн бага утга : ", B);
+} else if (C < A) {
+  console.log("хамгийн бага утга : ", C);
+} else if (C < B) {
+  console.log("хамгийн бага утга : ", C);
+}
+
+//daalgavar 5
+let mounth = Number(
+  prompt("Жилийн 12 сарын аль нэг сарын дугаарыг оруулна уу !!!")
+);
+if (mounth == 2 || mounth == 3 || mounth == 4) {
+  console.log("Хаврын улирал", mounth);
+}
+if (mounth == 5 || mounth == 6 || mounth == 7) {
+  console.log("Зуны улирал", mounth);
+}
+if (mounth == 8 || mounth == 9 || mounth == 10) {
+  console.log("Намрын улирал", mounth);
+}
+if (mounth == 11 || mounth == 12 || mounth == 1) {
+  console.log("Өвлийн улирал", mounth);
+}
+
+//daalgavar 6
+let XY = 85;
+let ZD = 75;
+let CB = 96;
+let SE = 69;
+switch()
